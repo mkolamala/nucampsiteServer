4cf7f4e5d38286ec6cd07e911b8ec65449d900b5
Krishna Kolamala <krishnakolamala318@gmail.com>
2020-02-22 01:43:26 -0800

    Passport JWT

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const User = require("../models/user");
 const passport = require('passport');
+const authenticate = require('../authenticate');
 const router = express.Router();
 
 /* GET users listing. */
@@ -8,24 +9,11 @@ router.get("/", function(req, res, next) {
   res.send("respond with a resource");
 });
 
-router.post('/signup', (req, res) => {
-  User.register(
-      new User({username: req.body.username}),
-      req.body.password,
-      err => {
-          if (err) {
-              res.statusCode = 500;
-              res.setHeader('Content-Type', 'application/json');
-              res.json({err: err});
-          } else {
-              passport.authenticate('local')(req, res, () => {
-                  res.statusCode = 200;
-                  res.setHeader('Content-Type', 'application/json');
-                  res.json({success: true, status: 'Registration Successful!'});
-              });
-          }
-      }
-  );
+router.post('/login', passport.authenticate('local'), (req, res) => {
+  const token = authenticate.getToken({_id: req.user._id});
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
