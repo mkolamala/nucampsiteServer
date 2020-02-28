292aa968eafc7ae94bbd5b7fda58a1e59620fae3
Krishna Kolamala <krishnakolamala318@gmail.com>
2020-02-28 05:45:03 -0800

    File Upload

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ var express = require("express");
 var path = require("path");
 var logger = require("morgan");
 const passport = require("passport");
-const config = require('./config');
+const config = require("./config");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var campsiteRouter = require("./routes/campsiteRouter");
 var promotionRouter = require("./routes/promotionsRouter");
 var partnerRouter = require("./routes/partnersRouter");
+const uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require("mongoose");
 
@@ -28,6 +29,20 @@ connect.then(
 
 var app = express();
 
+app.all("*", (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+    console.log(
+      `Redirecting to: https://${req.hostname}:${app.get("secPort")}${req.url}`
+    );
+    res.redirect(
+      301,
+      `https://${req.hostname}:${app.get("secPort")}${req.url}`
+    );
+  }
+});
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
@@ -36,8 +51,6 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser("12345-67890-09876-54321"));
-
-
 
 app.use(passport.initialize());
 
@@ -49,6 +62,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/campsites", campsiteRouter);
 app.use("/promotions", promotionRouter);
 app.use("/partners", partnerRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
